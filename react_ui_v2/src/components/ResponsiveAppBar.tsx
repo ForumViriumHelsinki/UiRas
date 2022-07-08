ef4d7eaf92152433787bf48866b60aff3e7bdbda
Aarni Koskela <akx@iki.fi>
2022-07-08 12:32:47 +0300

    Remove dead code from ResponsiveAppBar

--- react_ui_v2/src/components/ResponsiveAppBar.tsx
+++ react_ui_v2/src/components/ResponsiveAppBar.tsx
@@ -1,49 +1,20 @@
 import * as React from "react";
 
 import styled from "@emotion/styled";
-// import Map from '@mui/icons-material/Map';
 import Pool from "@mui/icons-material/Pool";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Menu from "@mui/material/Menu";
-// import Tooltip from '@mui/material/Tooltip';
-import MenuItem from "@mui/material/MenuItem";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
 import CustomizedDialogs from "./CustomizedDialogs";
-
-const pages = ["Kartta", "Lokikirja", "Info"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const FvhLogo = styled.img(() => ({
   maxHeight: "50px",
 }));
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorElUser(event.currentTarget);
-  // };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
     <AppBar position="static" sx={{ bgcolor: "rgb(236,94,36)" }}>
@@ -70,42 +41,6 @@ function ResponsiveAppBar() {
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <CustomizedDialogs />
-            {/* TODO: Waiting for implementation of the pages
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
- */}
           </Box>
           <Pool sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Typography
@@ -129,53 +64,12 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             <CustomizedDialogs />
           </Box>
-          {/* 
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-           */}
           <Box sx={{ flexGrow: 0 }}>
             <a href="https://forumvirium.fi">
               <FvhLogo
-                alt="Remy Sharp"
                 src="images/FORUM_VIRIUM_logo_white.png"
               />
             </a>
-            {/* <Tooltip title="Open settings"> */}
-            {/*  <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}> */}
-            {/*    <Avatar alt="Remy Sharp" src="images/FORUM_VIRIUM_logo_white.png"/> */}
-            {/*  </IconButton> */}
-            {/* </Tooltip> */}
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
