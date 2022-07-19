deb8fccb118528513e0ac528311771f1ae6dca3a
Aarni Koskela <akx@iki.fi>
2022-07-19 12:30:11 +0300

    Add some utility functions and hooks for near-future use

---
+++ react_ui/src/hooks/useMinMaxTemp.ts
@@ -0,0 +1,15 @@
+import React from "react";
+import { UirasFeature } from "../types/UiRaSGeoJSON";
+import { isValidFeature } from "../utils/validation";
+
+export default function useMinMaxTemp(
+  features: readonly UirasFeature[] | undefined
+): [number, number] {
+  return React.useMemo(() => {
+    if (!features) return [0, 0];
+    const temps = features
+      .filter(isValidFeature)
+      .map((feature) => feature.properties.temp_water);
+    return [Math.min(...temps), Math.max(...temps)];
+  }, [features]);
+}
