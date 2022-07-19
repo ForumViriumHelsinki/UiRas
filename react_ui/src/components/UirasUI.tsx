37a59c5f6850ddbfac22e07134ec8132edbbb24e
Aarni Koskela <akx@iki.fi>
2022-07-19 12:19:51 +0300

    Wire up react-router-dom

--- react_ui/src/components/UirasUI.tsx
+++ react_ui/src/components/UirasUI.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResponsiveAppBar from "./ResponsiveAppBar";
 import ResponsiveAppBarFooter from "./ResponsiveAppBarFooter";
 import UirasListAccordion from "./UirasListAccordion";
 
 export function UirasUI(): JSX.Element {
   return (
-    <>
+    <BrowserRouter>
       <ResponsiveAppBar />
-      <UirasListAccordion />
+      <Routes>
+        <Route index element={<UirasListAccordion />} />
+      </Routes>
       <ResponsiveAppBarFooter />
-    </>
+    </BrowserRouter>
   );
 }
