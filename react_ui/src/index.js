9a7bc421c1d9285675f17f9d75a72dffa2889277
Aarni Koskela <akx@iki.fi>
2022-08-04 12:25:58 +0300

    Remove legacy UI (#22)
    * Delete legacy react_ui
    
    * Rename react_ui_v2 to react_ui

--- react_ui/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import styles from './index.scss';
-import UiRasUI from "components/UiRasUI"; // eslint-disable-line
-
-ReactDOM.render(<UiRasUI/>, document.getElementById('root'));
